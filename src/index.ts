6fac9c35c34670cd92c6a043ad60a4416ecf0e6d
CarlosRuiz <carua97@gmail.com>
2023-02-24 12:06:01 +0100

    Add fixes errors server

--- src/index.ts
+++ src/index.ts
@@ -4,33 +4,52 @@ import url from 'url';
 
 const PORT = process.env.PORT || '8000';
 
+export class HTTPError extends Error {
+  statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
 const server = http.createServer((req, resp) => {
-  switch (req.method) {
-    case 'GET':
-      if (!req.url) {
-        server.emit('error', new Error('Invalid URL'));
-        return;
+  if (!req.url) {
+    const error = new HTTPError('Invalid URL', 404);
+    server.emit('error', error, resp);
+    return;
+  }
+
+  const { pathname, search } = url.parse(req.url);
+
+  switch (pathname) {
+    case '/calculator':
+      if (req.method !== 'GET') {
+        // Error 405
+        const error = new HTTPError('Método desconocido', 405);
+        server.emit('error', error, resp);
+        break;
       }
 
-      const { pathname, search } = url.parse(req.url);
+      const urlParams = new URLSearchParams(search!);
+      const a = Number(urlParams.get('a'));
+      const b = Number(urlParams.get('b'));
+      const sum = a + b;
+      const rest = a - b;
+      const multiply = a * b;
+      const divide = a / b;
 
-      if (pathname !== '/calculator') {
-        server.emit('error', new Error('Invalid URL'));
-        return;
+      if (isNaN(a) || isNaN(b)) {
+        server.emit(
+          'error',
+          new HTTPError('El dato introducido no es un número', 400),
+          resp
+        );
+        break;
       }
 
-      if (pathname === '/calculator') {
-        const urlParams = new URLSearchParams(search!);
-
-        const a = Number(urlParams.get('a'));
-        const b = Number(urlParams.get('b'));
-        const sum = a + b;
-        const rest = a - b;
-        const multiply = a * b;
-        const divide = a / b;
-        resp.writeHead(200, { 'Content-Type': 'text/html' });
-
-        resp.write(`
+      resp.writeHead(200, { 'Content-Type': 'text/html' });
+      resp.write(`
           <html>
             <head>
               <title>Calculator</title>
@@ -44,22 +63,36 @@ const server = http.createServer((req, resp) => {
             </body>
           </html>
         `);
-      }
 
       break;
-    case 'POST':
-    case 'PATCH':
-    case 'DELETE':
-      resp.write('No esta implementado ' + req.method);
+    case '/things':
+      server.emit('error', new Error('Invalid URL'));
       break;
+
     default:
-      resp.write('No conozco el método ' + req.method);
+      server.emit('error', new HTTPError('Error - path not found', 404), resp);
       break;
   }
 
   resp.end();
 });
-server.on('server', () => {});
+
+server.on('error', (error: Error | HTTPError, resp: http.ServerResponse) => {
+  // Errores HTTP
+  if (error instanceof HTTPError) {
+    console.log(error.statusCode, error.message);
+    resp.writeHead(200, { 'Content-Type': 'text/html' });
+    resp.write(`
+      <h1>Error</h1>
+      <p>${error.message}</p>
+      `);
+    resp.end();
+  } else {
+    console.log(error.message);
+  }
+
+  // Problemas técnicos
+});
 
 server.on('listening', () => {
   console.log('Listening in http://localhost:' + PORT);
